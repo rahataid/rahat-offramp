2ef71ae67f4412cc77bff8bacf0297b8919e9937
raghav-rumsan <raghav.kattel@rumsan.net>
2024-11-25 14:53:44 +0545

    add axios API client and define offramps endpoints

---
+++ lib/api.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5500/v1" + "/offramps",
+});
+
+export default api;
+
+export const endpoints = {
+  offramps: {
+    requests: {
+      create: "/",
+      list: "/",
+      execute: "/execute",
+    },
+    providers: {
+      list: "/providers",
+      register: "/providers",
+      actions: "/providers/actions",
+    },
+  },
+};
