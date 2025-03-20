9bacbfae5f6bd069a5ac664740f3c8f5a7154ff1
argahv <arg.ahv7@gmail.com>
2025-03-20 09:16:20 +0545

    feat: add MetaMask connector to wagmi configuration for enhanced wallet support

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,12 +1,17 @@
 import { createConfig, http } from "wagmi";
 import { customChain } from "./custom-chain";
-import { coinbaseWallet } from "wagmi/connectors";
+import { coinbaseWallet, metaMask } from "wagmi/connectors";
 // import { base } from "wagmi/chains";
 
 // Create wagmi config
 export const config = createConfig({
   syncConnectedChain: true,
-  connectors: [coinbaseWallet({})],
+  connectors: [
+    coinbaseWallet({}),
+    metaMask({
+      checkInstallationImmediately: true,
+    }),
+  ],
   chains: [customChain],
   transports: {
     // [mainnet.id]: http(),
